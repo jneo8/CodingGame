cc1db5e92629e99cd2158cb1ce168ec44b2c9569
jneo8 <jneo8lin@gmail.com>
2018-09-09 02:39:25 +0800

    95%

--- code-vs-zombies/main.go
+++ code-vs-zombies/main.go
@@ -15,24 +15,6 @@ type Hurnter struct {
 	y float64
 }
 
-func (h Hurnter) move(x float64, y float64) string {
-	needX := x - h.x
-	needY := y - h.y
-
-	// need := needX + needY
-	// if math.Abs(needX) + math.Abs(needY) > 1000.0 {
-	//      need = 1000.0
-	// }
-
-	moveX := 1000.0 * (needX / (needX + needY))
-	moveY := 1000.0 * (needY / (needX + needY))
-
-	finalX := int(h.x + moveX)
-	finalY := int(h.y + moveY)
-	result := fmt.Sprintf("%v %v", finalX, finalY)
-	return result
-}
-
 type Human struct {
 	id int
 	x  float64
@@ -49,42 +31,7 @@ type Zombie struct {
 
 type Humans []Human
 
-func (humans Humans) centerPoint() (float64, float64) {
-	totalX := 0.0
-	totalY := 0.0
-	for _, h := range humans {
-		totalX += h.x
-		totalY += h.y
-	}
-
-	x := totalX / float64(len(humans))
-	y := totalY / float64(len(humans))
-	return x, y
-}
-
 type Zombies []Zombie
-
-func (zombies Zombies) centerPoint() (float64, float64) {
-	totalX := 0.0
-	totalY := 0.0
-	for _, z := range zombies {
-		totalX += z.x
-		totalY += z.y
-	}
-
-	x := totalX / float64(len(zombies))
-	y := totalY / float64(len(zombies))
-	return x, y
-}
-
-func stringInSlice(a int, list []int) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
-}
 
 func distance(p1X float64, p1Y float64, p2X float64, p2Y float64) float64 {
 	return math.Pow(math.Pow(p1X-p2X, 2)+math.Pow(p1Y-p2Y, 2), 0.5)
