923114cbf24e342656f32ccb3cd707c59bedce3c
jneo8 <jneo8lin@gmail.com>
2018-09-08 22:56:00 +0800

    func human center point

--- code-vs-zombies/main.go
+++ code-vs-zombies/main.go
@@ -8,22 +8,37 @@ import "os"
  **/
 
 type Hurnter struct {
-	x int
-	y int
+	x float64
+	y float64
 }
 
 type Human struct {
-	id int
-	x  int
-	y  int
+	id float64
+	x  float64
+	y  float64
 }
 
 type Zombie struct {
-	id    int
-	xNext int
-	yNext int
-	x     int
-	y     int
+	id    float64
+	xNext float64
+	yNext float64
+	x     float64
+	y     float64
+}
+
+type Humans []Human
+
+func (humans Humans) centerPoint() (float64, float64) {
+	totalX := 0.0
+	totalY := 0.0
+	for _, h := range humans {
+		totalX += h.x
+		totalY += h.y
+	}
+
+	x := totalX / float64(len(humans))
+	y := totalY / float64(len(humans))
+	return x, y
 }
 
 func main() {
@@ -31,25 +46,27 @@ func main() {
 		var x, y int
 		fmt.Scan(&x, &y)
 
-		hurnter := &Hurnter{x: x, y: y}
+		hurnter := &Hurnter{x: float64(x), y: float64(y)}
 		fmt.Fprintf(os.Stderr, "Hurnter: %#v\n", hurnter)
 
 		var humanCount int
 		fmt.Scan(&humanCount)
 
-		humans := []Human{}
+		humans := Humans{}
 		for i := 0; i < humanCount; i++ {
 			var humanId, humanX, humanY int
 			fmt.Scan(&humanId, &humanX, &humanY)
 			human := Human{
-				id: humanId,
-				x:  humanX,
-				y:  humanY,
+				id: float64(humanId),
+				x:  float64(humanX),
+				y:  float64(humanY),
 			}
 			humans = append(humans, human)
 		}
 
 		fmt.Fprintf(os.Stderr, "humans: %#v\n", humans)
+		centerX, centerY := humans.centerPoint()
+		fmt.Fprintf(os.Stderr, "humans centerpoint: %v %v\n", centerX, centerY)
 
 		var zombieCount int
 		fmt.Scan(&zombieCount)
@@ -60,11 +77,11 @@ func main() {
 			var zombieId, zombieX, zombieY, zombieXNext, zombieYNext int
 			fmt.Scan(&zombieId, &zombieX, &zombieY, &zombieXNext, &zombieYNext)
 			zombie := Zombie{
-				id:    zombieId,
-				xNext: zombieXNext,
-				yNext: zombieYNext,
-				x:     zombieX,
-				y:     zombieY,
+				id:    float64(zombieId),
+				xNext: float64(zombieXNext),
+				yNext: float64(zombieYNext),
+				x:     float64(zombieX),
+				y:     float64(zombieY),
 			}
 			zombies = append(zombies, zombie)
 		}
